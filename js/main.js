c19e350746aa9e5b25bba6f2cec33b8b63129e02
Ines-1958 <79308453+Ines-1958@users.noreply.github.com>
2021-12-30 17:55:19 +0100

    création du nouveau select dans le DOM

--- js/main.js
+++ js/main.js
@@ -7,14 +7,151 @@ async function  getRecipies () {
    return json.recipies;
 }
 
+//Fonction pour la création du select avec les données json
+async function monSelect () {
+    const sectionSelect = document.querySelector(".select");
+    const recipies = await getRecipies();
+
+    var monHtml = `
+        <div class="select-filter">
+                <div class="select-filter__item">
+                    <select  name="" class="ingredients" id="">
+                        <option value="">Ingredients</option>
+                    `;
+                    const ingredients = [].concat.apply([], recipies.map((recipie) => recipie.ingredients));
+                    console.log(ingredients)
+                    const uniqueIngredients = ingredients.filter((value, index, self) => index === self.findIndex((t) => t.ingredient.toLowerCase() === value.ingredient.toLowerCase()))
+                    console.log(uniqueIngredients)
+                    uniqueIngredients.forEach(i => monHtml +=  `<option value="${i.ingredient}">${i.ingredient}</option>`) 
+                     monHtml += ` </select>
+                    <!--<div class="selected-items">
+                        <div class="select-filter-selected ingredient-selected">coco <i class="far fa-times-circle"></i></div>
+                        <div class="select-filter-selected appareil-selected">blender <i class="far fa-times-circle"></i></div>
+                        <div class="select-filter-selected casserole-selected">casseroles <i class="far fa-times-circle"></i></div>
+                    </div>-->
+    </div>
+    </div>`;
+    sectionSelect.innerHTML += monHtml; 
+}
+monSelect();
+
+window.onload = () => {
+    //Récupération du select
+const selectElement = document.querySelector("select");
+console.log(selectElement)
+
+//Récupurétion 1ère div "select-filter"
+const selectDiv = document.querySelector(".select-filter__item");
+
+//CREATION DE TABLEAUX POUR LES ELEMENTS SELECTIONNES
+const selectedItems = document.createElement("div");
+selectedItems.classList.add("selected-items");
+selectDiv.appendChild(selectedItems);
+
+//Création div et icone ingredient
+const selectFilterIngredient = document.createElement("div");
+selectFilterIngredient.classList.add("select-filter-selected", "ingredient-selected" );
+selectedItems.appendChild(selectFilterIngredient);
+const iconeIngredient = document.createElement("i")
+iconeIngredient.classList.add("far", "fa-times-circle");
+selectFilterIngredient.appendChild(iconeIngredient);
+
+//Création div et icone Appareil
+const selectFilterAppareil = document.createElement("div");
+selectFilterAppareil.classList.add("select-filter-selected", "appareil-selected" );
+selectedItems.appendChild(selectFilterAppareil);
+const iconeAppareil = document.createElement("i")
+iconeAppareil.classList.add("far", "fa-times-circle");
+selectFilterAppareil.appendChild(iconeAppareil);
+
+//Création div et icone Ustensiles
+const selectFilterUstensile = document.createElement("div");
+selectFilterUstensile.classList.add("select-filter-selected", "ustensile-selected" );
+selectedItems.appendChild(selectFilterUstensile);
+const iconeUstensile = document.createElement("i")
+iconeUstensile.classList.add("far", "fa-times-circle");
+selectFilterUstensile.appendChild(iconeUstensile);
+
+
+
+//FIN DU TABLEAU
+
+//Création nouveau select
+//const newFilterSelected = document.querySelector(".ingredient-selected");
+const newFilterSelected = document.createElement("div");
+
+//Ajout de la classe "new-select"
+//newFilterSelected.classList.add("select-filter__donnees");
+newFilterSelected.classList.add("select-filter-selected", "ingredient-selected");
+console.log(newFilterSelected)
+
+//Ajout de l'id sur l'élément
+//newFilterSelected.setAttribute("id", "ingredients");
+
+//Ajout de l'option actuellement choisie dans le select
+newFilterSelected.innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML;
+//Création de l'élément dans le DOM
+selectDiv.appendChild(newFilterSelected);
+
+const selectIngredients = document.createElement("div");
+selectIngredients.classList.add("select-filter__donnees");
+selectIngredients.setAttribute("id", "ingredients");
+
+const searchIngredient = document.createElement("div");
+searchIngredient.classList.add("select-filter__donnees--label")
+
+
+selectIngredients.appendChild(searchIngredient);
+selectDiv.appendChild(selectIngredients)
+console.log(selectIngredients)
+
+//Création menu déroulant et ajout de classe et id
+const menuDeroulant = document.createElement("ul");
+menuDeroulant.classList.add("select-items", "select-hide");
+menuDeroulant.setAttribute("id", "ul-ingredients");
+
+//Boucle sur les options dans le select et les copier dans la div
+    for(let option of selectElement.options) {
+        //console.log(option)
+        //Création div pour cette option
+        const newOption = document.createElement("li");
+    
+        //on copie le contenu de l'option
+        newOption.innerHTML = option.innerHTML;
+        
+        newOption.setAttribute("data-filter", option.innerHTML);
+
+        //Ajout de l'option dans le menu Deroulant
+        menuDeroulant.appendChild(newOption);
+
+    }
+//selectDiv.appendChild(menuDeroulant);
+selectIngredients.appendChild(menuDeroulant);
+
+//Ajout de l'écouteur d'événements click sur newFilterSelected
+newFilterSelected.addEventListener("click", function(e) {
+    e.stopPropagation();
+    console.log(this)
+    //retrait du select-hide du menu
+    this.nextSibling.classList.toggle("select-hide");
+    //this.classList.add("select-hide");
+    
+    //ajout de la classe active à newFilterSelected pour changer le sens de la flèche
+    this.classList.toggle("active");
+})
+
+
+}
+
+
 
 async function mesRecettes () {
     const section = document.querySelector('.recettes');
     const recipies = await getRecipies();
     recipies.forEach(function(recettes) {
-        console.log(recettes)
+        
          var myHtml = `
-         <div class="recettes-bloc">
+         <div class="recettes-bloc" id="recette-${recettes.id}">
             <a href="recettes.html?id=${recettes.id}">
                 <div class="recettes-bloc__heading"></div>
                 <div class="recette">
@@ -38,6 +175,7 @@ mesRecettes();
 
 function buildDescription (ingredients) {
     return ingredients.map( ingredient => {
+        //console.log(ingredient.ingredient)
         if (ingredient.unit !== undefined) {
             return `<p class="recette__ingredients--p"><span class="recette__ingredients--p--span">${ingredient.ingredient}</span>: <span>${ingredient.quantity} ${ingredient.unit}</span></p>`;   
         }
@@ -48,4 +186,24 @@ function buildDescription (ingredients) {
             return `<p class="recette__ingredients--p"><span class="recette__ingredients--p--span">${ingredient.ingredient}</span></p>`;  
         }
     }
-)}
+).join('')}
+
+
+
+//Fonction pour filtre des cards
+const filtreCards = document.getElementById("filtre-cards");
+console.log("mamamamam")
+async function cardFilter () {
+    console.log("toto")
+    const recipies = await getRecipies();
+    filtreCards.addEventListener("input", function() {
+        let saisie = this.value.toLowerCase().trim();//trim pour supprimer les espaces dans la saisie
+    
+        recipies.forEach((card) => {
+            console.log(`recette-${card.id}`);
+            var carteFiltre = document.getElementById(`recette-${card.id}`);
+            carteFiltre.style.display = (card.name.toLowerCase().trim().includes(saisie)) ? "block" : "none";//fonction ternaire:if, display block, else, display none
+        });
+    });
+}
+cardFilter();
--- select.js
+++ select.js
@@ -30,8 +30,176 @@ async function menuSelect () {
     const recipies = await getRecipies();
     recipies.ingredients.forEach(function(mesIngredients) {
         console.log(mesIngredients)
-         var myHtml = `
+         var monHtml = ` 
+         <div class="select-filter">
+            <div class="select-filter__item ">
+                <select  name="" class="ingredients" id="">
+                    <option value="">Ingredients</option>
+                    <option value="lait de coco">Lait de coco</option>
+                    <option value="sucre">Sucre</option>
+                    <option value="concombre">Concombre</option>
+                </select>
+            </div>
+            <div class="select-filter__item ">
+                <select  name="" id="" class="appareil">
+                    <option value="">Appareil</option>
+                    <option value="blender">Blender</option>
+                </select>
+            </div>
+            <div class="select-filter__item" >
+                <select name="" id="" class="ustensiles">
+                    <option value="">Ustensiles</option>
+                    <option value="casseroles">Casseroles</option>>
+                </select>
+            </div>
+         </div>
          `
         }
      )}
     }
+
+    function build (ingredients) {
+        return ingredients.map(ingredient => 
+        `<p class="recette__ingredients--p"><span class="recette__ingredients--p--span">${ingredient.ingredient}</span>: <span>${ingredient.quantity}</sapn></p>`).join('');
+    }
+
+    const selectElt = document.querySelector("select"); //récupération select
+
+    const selectDiv = document.querySelector(".select-filter__item")//selectFilter
+
+    const newSelect = document.querySelector(".select-filter__donnees--label")// <div id="ingredients" class="select-filter__donnees"> ::after, select-items div { border-bottom} select-hide{display:none}
+
+    /*newSelect.classList.add("new-select")*///class="select-filter__donnees"
+
+    //contenu de l'option choisie
+    newSelect.innerHTML = selectElt.options[selectElt.selectedIndex].innerHTML;
+
+    //création élément dans le DOM
+    /*selectDiv.appendChild(newSelect)*/
+
+    //création menu déroulant
+    const newMenu = document.querySelector("ul")//ul
+    /*newMenu.classList.add("select-items", "select-hide")*/
+
+    //boucle sur toutes options dans le select et les copier dans la div
+    for(let option of selectElt.option) {
+        //création div pour cette option
+        const newOption = document.querySelector(".select-item");//li
+
+        //on copie le contenu de l'option
+        newOption.innerHTML = option.innerHTML;
+
+        //on ajoute un écouteur d'événements "clic" sur l'option
+        newOption.addEventListener("click", function() {
+            //on boucle sur chacune des options du select original
+            for(let option of selectElt.options) {
+                if(option.innerHTML === this.innerHTML){//si le contenu de mon option correspond à l'option sur lequel je clique 
+                    //on active la bonne option dans le select
+                    selectElt.selectedIndex = option.index;
+
+                    //on change le texte de notre newSelect
+                    newSelect.innerHTML = this.innerHTML;
+                    break;
+                }
+            }
+            //on simule un clic sur newSelect
+            newSelect.click();//pour fermer le menu
+        })
+
+        //on ajoute l'option dans le newMenu
+        /*newMenu.appendChild(newOption);*/
+    }
+
+
+    //afficher menu
+    selectDiv.appendChild(newMenu)
+
+    //on ajoute l'écouteur d'événements click sur newSelect
+    newSelect.addEventListener("click", function(e) {
+        //on empêche la propagation du clic
+        e.stopPropagation();
+
+        //on retire le "select-hide" de notre menu
+        this.nextSibling.classList.toggle("select-hide")
+
+        //on ajoute la classe active à newSelect(changer la flèche)
+        this.classList.toggle("active") //.new-select.active::after{border-color, top}
+
+        //on modifie l'attribut contenteditable pour pouvoir taper une valeur
+        if(this.getAttribute("contenteditable") == "true") {
+            this.setAttribute("contenteditable", "false");
+        }
+        else {
+            this.setAttribute("contenteditable", "true");
+            //on donne le focus à notre champ
+            this.focus();
+        }
+
+
+    })
+
+    //on met en place le filtre de contenu sur l'événement  input
+    newSelect.addEventListener("input", function() {
+        //on récupère la saisie en minuscules
+        let saisie = this.textContent.toLowerCase();
+
+        //on parcourt tous les enfants de notre menu(newMenu)
+        for(let option of newMenu.children) {
+            //on vérifie si la saisie existe dans la chaîne
+            if(option.textContent.toLowerCase().search(saisie) > -1) {//-1 pcq search renvoie -1 s'il ne trouve pas
+                option.style.display = "block";
+            }
+            else {
+                option.style.display = "none";
+            }
+        }
+    })
+
+    const filtreCards = document.getElementById("filtre-cards");
+    //filtreCards.addEventListener("input", cardsFiltres)
+    filtreCards.addEventListener("input", function() {
+        const recipies = await getRecipies();
+        let saisie = this.textContent.toLocaleLowerCase();
+
+        recipies.forEach(function(cards) {
+            if(cards.textContent.toLowerCase().filter(saisie) !== -1){
+                cards.style.display = "block";
+            }
+            else {
+                cards.style.display = "none";
+            }
+        })
+        
+    })
+
+    function filtreTexte(arr, requete) {
+        return arr.filter(function (el) {
+          return el.toLowerCase().indexOf(requete.toLowerCase()) !== -1;
+        })
+      }
+
+      function filtrerParID(obj) {
+        // Si c'est un nombre
+        if (obj.id !== undefined && typeof(obj.id) === 'number' && !isNaN(obj.id)) {
+          return true;
+        } else {
+          elementsInvalides++;
+          return false;
+        }
+      }
+      
+      var arrByID = arr.filter(filtrerParID);
+
+      function filtrerParName(json) {
+        return json.filter(function (el) {
+            return el.toLowerCase().indexOf(requete.toLowerCase()) !==-1;
+        })
+      }
+
+
+
+
+
+
+
+
